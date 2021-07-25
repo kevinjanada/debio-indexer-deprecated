66340f448ee0dc1f1ebb2d917b8f929d5281311a
Kevin Janada <kevin.janada@gmail.com>
2021-07-25 21:16:47 +0700

    feat: Index Order data on OrderCreated events

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export * from './mappings/mappingHandlers'
 export * from './mappings/labsHandlers'
 export * from './mappings/servicesHandlers'
+export * from './mappings/ordersHandlers'
---
+++ src/mappings/ordersHandlers.ts
@@ -0,0 +1,75 @@
+import {SubstrateEvent} from "@subql/types"
+import {Order} from '../types'
+
+const OrderCreated = 'OrderCreated'
+const OrderPaid = 'OrderPaid'
+const OrderSuccess = 'OrderSuccess'
+const OrderRefunded = 'OrderRefunded'
+const OrderCancelled = 'OrderCancelled'
+const OrderNotFound = 'OrderNotFound' // Does not need to be inserted to DB
+const OrderFailed = 'OrderFailed'
+
+export async function handleOrderEvents(event: SubstrateEvent): Promise<void> {
+  const {event: {data: [order]}} = event
+  const eventMethod = event.event.method
+  logger.info(`Orders Event -> ${eventMethod}`)
+
+  const orderData = order.toJSON()
+
+  switch(eventMethod) {
+    case OrderCreated:
+      await onOrderCreated(orderData)
+      break
+    case OrderPaid:
+      await onOrderPaid(orderData)
+      break
+    case OrderSuccess:
+      await onOrderSuccess(orderData)
+      break
+    case OrderRefunded:
+      await onOrderRefunded(orderData)
+      break
+    case OrderCancelled:
+      await onOrderCancelled(orderData)
+      break
+    case OrderFailed:
+      await onOrderFailed(orderData)
+      break
+  }
+}
+
+async function onOrderCreated(orderData: any): Promise<void> {
+  const order = new Order(orderData['id'])
+  order.service_id = orderData['service_id']
+  order.customer_id = orderData['customer_id']
+  order.customer_box_public_key = orderData['customer_box_public_key']
+  order.seller_id = orderData['seller_id']
+  order.dna_sample_tracking_id = orderData['dna_sample_tracking_id']
+  order.currency = orderData['currency']
+  order.prices = orderData['prices']
+  order.additional_prices = orderData['additional_prices']
+  order.status = orderData['status']
+  order.created_at = orderData['created_at']
+  order.updated_at = orderData['updated_at']
+  await order.save()
+}
+
+async function onOrderPaid(orderData: any): Promise<void> {
+
+}
+
+async function onOrderSuccess(orderData: any): Promise<void> {
+
+}
+
+async function onOrderRefunded(orderData: any): Promise<void> {
+
+}
+
+async function onOrderCancelled(orderData: any): Promise<void> {
+
+}
+
+async function onOrderFailed(orderData: any): Promise<void> {
+
+}
